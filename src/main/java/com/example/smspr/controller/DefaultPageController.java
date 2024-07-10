b018f3889e1bbc48a91e774aa0d397bf7429de4b
arbi99q <arbi99q@gmail.com>
2024-07-10 10:17:28 +0900

    go

--- src/main/java/com/example/smspr/controller/DefaultPageController.java
+++ src/main/java/com/example/smspr/controller/DefaultPageController.java
@@ -8,17 +8,33 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.HashMap;
 import java.util.Map;
 
-@RequestMapping("")
+@RequestMapping("") //무조건 선언해야함
 @Controller
 public class DefaultPageController {
 
+    //2024-07-09
     @GetMapping("/index")
     public String index(){
         return "index";
     }
+
+    //아무것도 안 입력했을 때도 index로 가게함
+    @GetMapping({"","/","index2"})
+    public String empty(){
+        //return "index";
+        ///index로 바꿔줌
+        return "redirect:/index";
+    }
+
     @GetMapping("/test")
     public String test(){
         return "test";
+    }
+
+    //2024-07-10
+    @GetMapping("/page1")
+    public String page1(){
+        return "page1";
     }
 
 
---
+++ src/main/java/com/example/smspr/controller/TbpostPageController.java
@@ -0,0 +1,11 @@
+package com.example.smspr.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RequestMapping("")
+@Controller
+public class TbpostPageController {
+
+}
